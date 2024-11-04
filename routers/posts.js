673f8bf2ae2f6452aaee8fd915d9d68182a579fb
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-04 16:50:45 +0100

    add route

--- routers/posts.js
+++ routers/posts.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const PostController = require('../controllers/PostController.js')
 
 router.get('/', PostController.index)
+router.get('/:slug', PostController.show)
 
 module.exports = router
